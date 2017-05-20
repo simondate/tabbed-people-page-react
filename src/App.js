9bf8de8b0869a8e2399687ff645ac216ed3a7c39
Simon Date <simondate@yahoo.co.uk>
2017-05-20 19:54:09 +0100

    Removed code and image from the tutorial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Tabs from './Tabs.js';
 import Textbox from './Textbox.js';
@@ -20,18 +19,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
         <div className="hero-image"/>
         <div className="wrapper">
           <Tabs changeTab={this.changeTab.bind(this)}/>
           <Textbox current={this.state.currentTab}/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
