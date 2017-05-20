2e26c29ab9b58d5f5cf7a482c2bf57979fa35f0e
Simon Date <simondate@yahoo.co.uk>
2017-05-20 15:33:16 +0100

    Added textbox component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Tab from './Tabs.js';
+import Textbox from './Textbox.js';
 
 class App extends Component {
   render() {
@@ -12,8 +13,10 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <div className="hero-image">s</div>
-        <Tab />
-        <div className="tabs"> </div>
+        <div className="wrapper">
+          <Tab />
+          <Textbox />
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/Textbox.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Textbox extends Component {
+  render() {
+    return (
+      <div className="textbox">
+        <div className="textbox-inner">My text goes here </div>
+      </div>
+    );
+  }
+}
+
+export default Textbox;
