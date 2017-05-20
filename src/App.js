c8abd7503bbe5dc8c8b2b9dc3c3dc380a99cb3a9
Simon Date <simondate@yahoo.co.uk>
2017-05-20 14:49:16 +0100

    Added hero image to the page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <div className="hero-image">s</div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
